2374a0463116a59ccb5d3eccf40075ceea1b5601
Rinch WU <whf535802393@gmail.com>
2024-05-02 17:54:40 +0800

    update

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -5,6 +5,11 @@
 // I AM NOT DONE
 
 pub fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
