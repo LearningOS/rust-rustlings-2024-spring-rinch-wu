64bb098205e25ba2cf422d8459e5b5785e2b284a
Rinch WU <whf535802393@gmail.com>
2024-06-04 09:38:12 +0800

    exec: update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x: i32) -> i32 {
+    if x > 40 {
+        x
+    } else {
+        x * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
